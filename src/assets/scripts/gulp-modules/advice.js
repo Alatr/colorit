d2a052a2623dcb3198bd32c59c0330236e94f70f
Alatr <dimalukianovweb@gmail.com>
2020-08-14 14:43:14 +0300

    lists news,obj,advice, prod-card

---
+++ src/assets/scripts/gulp-modules/advice.js
@@ -0,0 +1,11 @@
+
+(function ($) {
+	const tab = new Tab({
+		$btn: $('.js-advice-tab-btn'),
+		$content: $('.js-advice-tab-content-item'),
+		btnActiveClass: 'advice-btn-content-item--active',
+		contentActiveClass: 'advice-tab-content-item--active',
+		active: 0
+	});
+
+})(jQuery);
--- src/assets/scripts/libs/libs.js
+++ src/assets/scripts/libs/libs.js
@@ -1,15 +1,5 @@
 @@include('../libs/jquery/jquery.min.js')
-
 @@include('../libs/gsap/gsap.js')
 @@include('../libs/scroll_magic/scroll_magic.js')
-
 @@include('../libs/pub_sub/pub_sub.js')
-
-
-
-
-
 @@include('../libs/slick/slick.js')
-
-
-
