cfe8e49e94c174616e24d976ba9a83278425640e
Artem Tsys <19successful91@gmail.com>
2020-10-02 13:35:48 +0300

    fix search

--- src/assets/scripts/gulp-modules/palette-search.js
+++ src/assets/scripts/gulp-modules/palette-search.js
@@ -1,83 +1,87 @@
-let currentColors
-let currentSelectPalette
-let textForSearch = ''
-const search = document.querySelector('[data-search="palette"]')
-const resultContainer = document.querySelector('.pallete-search__result-list')
-const searchWrapper = document.querySelector('.js-pallete-search-wrapper') 
-const searchTrigger = document.querySelector('.js-pallete-search__btn')
-const palitteRes = document.querySelector('.pallete-search__result-text')
+class PaletteSearch{
+    constructor(){
+        this.currentColors = null;
+        this.currentSelectPalette = null;
+        this.textForSearch = '';
+        this.resultContainer = document.querySelector('.pallete-search__result-list');
+        this.searchWrapper = document.querySelector('.js-pallete-search-wrapper');
+        this.searchTrigger = document.querySelector('.js-pallete-search__btn');
+        this.palitteRes = document.querySelector('.pallete-search__result-text');
+    }
 
-function getColors({colors, name}) {
-    currentColors = colors
-    currentSelectPalette = name
-}
+    getColors(colors, name) {
+        this.currentColors = colors
+        this.currentSelectPalette = name
+    }
 
-function templatePalette(data) {
-    return `
-    <li class="pallete-search__result-item"> <a href="/palette?style=${currentSelectPalette}&color=${data.name.replace(" ", '-')}"> 
+    templatePalette(data) {
+        return `
+    <li class="pallete-search__result-item"> <a href="/palette?style=${this.currentSelectPalette}&color=${data.name.replace(' ', '-')}"> 
         <div class="pallete-search__color" style="background: rgb(${data.r}, ${data.g}, ${data.b})"></div><span class="pallete-search__color-name"> ${data.name}</span></a>
     </li>
     `
-}
+    }
 
-function renderList(filteredColors) {
-   resultContainer.innerHTML = ''
+    renderList(filteredColors) {
+        this.resultContainer.innerHTML = ''
 
-   if (!filteredColors.length) {
-        palitteRes.textContent = 'Краски не найденны'
-        return
-   }
-
-   palitteRes.textContent = 'Результат:'
-   const items =  filteredColors.map(templatePalette).join('')
-   resultContainer.insertAdjacentHTML('afterbegin', items)
-}
-
-function searchColors(text) {
-    let colorsFound = []
-
-    currentColors.colors.forEach(function(color) {
-        const name = removeSpaceInStr(color.name.toLowerCase())
-
-        if (name.indexOf(text, 0) !== -1) {
-            if(colorsFound.length >= 5) {
-                return
-            }
-
-            colorsFound.push(color)
+        if (!filteredColors.length) {
+            this.palitteRes.textContent = 'Краски не найденны'
+            return
         }
-    })
 
-    renderList(colorsFound)
-    searchWrapper.classList.add('pallete-search-wrapper_result-visible');
-}
-
-function removeSpaceInStr(str) {
-   return str = str.replace(/\s/g, '');
-}
-
-search.addEventListener('keyup', (e) => {
-    let value = e.target.value
-    if (value.length >= 3) {
-        textForSearch = value
-        searchColors(removeSpaceInStr(textForSearch.toLowerCase()))
-    } else {
-        searchWrapper.classList.remove('pallete-search-wrapper_result-visible');
+        this.palitteRes.textContent = 'Результат:'
+        const items =  filteredColors.map(this.templatePalette).join('')
+        this.resultContainer.insertAdjacentHTML('afterbegin', items)
     }
-})
 
-searchTrigger.addEventListener('click', function(e){
-    e.preventDefault();
-    e.stopPropagation()
+    searchColors(text) {
+        let colorsFound = []
 
-    if(textForSearch >= 4) { 
-        searchColors(removeSpaceInStr(textForSearch.toLowerCase()))
-        searchWrapper.classList.add('pallete-search-wrapper_result-visible');
+        this.currentColors.colors.forEach(function(color) {
+            const name = this.removeSpaceInStr(color.name.toLowerCase())
+
+            if (name.indexOf(text, 0) !== -1) {
+                if(colorsFound.length >= 5) return;
+                colorsFound.push(color)
+            }
+        })
+
+        this.renderList(colorsFound)
+        this.searchWrapper.classList.add('pallete-search-wrapper_result-visible');
     }
-})
 
-document.addEventListener('click', function(e){
-    if (e.target !== searchWrapper && searchWrapper.classList.contains('pallete-search-wrapper_result-visible')) {
-        searchWrapper.classList.remove('pallete-search-wrapper_result-visible');
+    removeSpaceInStr(str) {
+        return str.replace(/\s/g, '');
     }
-})
+
+    init(){
+        document.querySelector('[data-search="palette"]').addEventListener('keyup', (e) => {
+            let value = e.target.value;
+            if (value.length >= 3) {
+                this.textForSearch = value
+                this.searchColors(this.removeSpaceInStr( this.textForSearch.toLowerCase()))
+            } else {
+                this.searchWrapper.classList.remove('pallete-search-wrapper_result-visible');
+            }
+        })
+
+        this.searchTrigger.addEventListener('click', function(e){
+            e.preventDefault();
+            e.stopPropagation()
+
+            if( this.textForSearch >= 4) {
+                this.searchColors(this.removeSpaceInStr( this.textForSearch.toLowerCase()))
+                this.searchWrapper.classList.add('pallete-search-wrapper_result-visible');
+            }
+        })
+
+        document.addEventListener('click', function(e){
+            if (e.target !==  this.searchWrapper &&  this.searchWrapper.classList.contains('pallete-search-wrapper_result-visible')) {
+                this.searchWrapper.classList.remove('pallete-search-wrapper_result-visible');
+            }
+        })
+    }
+
+};
+
