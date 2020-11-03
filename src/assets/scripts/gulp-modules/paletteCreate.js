37e1eb11f6c7ccd34018b3d5f46e25ea87cded95
Artem Tsys <19successful91@gmail.com>
2020-11-03 14:17:04 +0200

    fix pallete link labg

--- src/assets/scripts/gulp-modules/paletteCreate.js
+++ src/assets/scripts/gulp-modules/paletteCreate.js
@@ -10,10 +10,10 @@ class PaletteCreate {
     }
 
     init(type) {
-
+        this.lang = document.querySelector('html').lang
         this.productChangeListeners(); // add custom event for onload json color
-        this.getColors('/assets/jsons/','colorCode', (reject) => {
-        // this.getColors('/wp-content/themes/kolorit/assets/jsons/','colorCode', (reject) => {
+        // this.getColors('/assets/jsons/','colorCode', (reject) => {
+        this.getColors('/wp-content/themes/kolorit/assets/jsons/','colorCode', (reject) => {
             this.tabsInfo = reject;
         });
 
@@ -27,8 +27,8 @@ class PaletteCreate {
         this.type = type;
         this.tab = tabs;
         this.description = description;
-        this.getColors('/assets/jsons/',type, this.setContentPalette.bind(this));
-        // this.getColors('/wp-content/themes/kolorit/assets/jsons/',type, this.setContentPalette.bind(this));
+        // this.getColors('/assets/jsons/',type, this.setContentPalette.bind(this));
+        this.getColors('/wp-content/themes/kolorit/assets/jsons/',type, this.setContentPalette.bind(this));
 
         $('.js-palette-tab').on('DOMMouseScroll wheel', '.js-horizontal-scroll',function (e) {
             let delta = e.originalEvent.deltaY;
@@ -158,6 +158,7 @@ class PaletteCreate {
         `;
 
         tabsInfo.forEach( (name, i) => {
+
             selected += `<option class="palette-item__select-item ${ i === 0 ? selected='selected':'' }" >${name.name[this.lang]}</option>`;
             buttons += `<li class="palette-tab-btns-item">
                         <button class="palette-tab-btn js-palette-color-tab-btn btn--res" type="button">${name.name[this.lang]}<span class="line"></span></button>
@@ -261,7 +262,7 @@ class PaletteCreate {
         let name = el.name.replace(' ', '-');
         return `
             <li class='color-shade-item' style="background: rgb(${el.r},${el.g},${el.b})">
-                <a href='/palette?style=${this.type}&color=${name}' ></a>
+                <a href='/${this.lang}/palette?style=${this.type}&color=${name}' ></a>
             </li>
         `
     }
